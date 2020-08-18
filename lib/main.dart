5b59e39e20d098220634e7049e50b6905ff3000c
gabber78 <gabor.rab@gmail.com>
2020-08-18 09:02:06 +0200

    1st solution done

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+        centerTitle: true,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void changeImage() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      //Creates a widget that centers its child.
+      child: FlatButton(
+        onPressed: () {
+          changeImage();
+          //print('$ballNumber');
+        },
+        child: Image(
+          image: AssetImage('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
